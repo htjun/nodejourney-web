2b6b1b699697bca3af41528f3732878202ecc740
Jason Jun <uxd.jun@gmail.com>
2026-01-02 08:35:21 +1100

    Shorten sticky header CTA button text to 'Download'

--- components/sticky-header.tsx
+++ components/sticky-header.tsx
@@ -44,7 +44,7 @@ export function StickyHeader({ downloadUrl }: StickyHeaderProps) {
       <div className="flex items-center justify-between w-full max-w-7xl mx-auto px-4 py-3">
         <Logo />
         <CtaButton size="xs" href={downloadUrl} colors={IMAGE_THEMES[imageIndex].colors}>
-          Download for macOS
+          Download
         </CtaButton>
       </div>
     </div>
